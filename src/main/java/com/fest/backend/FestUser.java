e7c08f1dd8ac8704e6705b729f8e96152f61afe3
Vinay Kumar J <jvkumar2003@gmail.com>
2025-06-27 21:03:02 +0530

    changes

--- src/main/java/com/fest/backend/FestUser.java
+++ src/main/java/com/fest/backend/FestUser.java
@@ -6,11 +6,21 @@ import jakarta.persistence.Id;
 
 @Entity
 public class FestUser {
+
+
     @Id
     @GeneratedValue
     private Integer id;
     private String email;
     private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getEmail() {
         return email;
