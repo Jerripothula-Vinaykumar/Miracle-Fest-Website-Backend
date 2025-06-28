6049825bca09421e97424881bf887c23ecb16987
Vinay Kumar J <jvkumar2003@gmail.com>
2025-06-28 12:26:31 +0530

    table

--- src/main/java/com/fest/backend/FestUser.java
+++ src/main/java/com/fest/backend/FestUser.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GenerationType;
 
 import jakarta.persistence.Id;
 
+import java.math.BigInteger;
+
 @Entity
 public class FestUser {
 
@@ -13,8 +15,99 @@ public class FestUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    private String username;
+    private String year;
+    private String branch;
+    private String section;
+    private String mobileno;
+    private String linkedin;
+    private String instaid;
+    private String village;
+    private String district;
+    private String state;
     private String email;
     private String password;
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getInstaid() {
+        return instaid;
+    }
+
+    public void setInstaid(String instaid) {
+        this.instaid = instaid;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getMobileno() {
+        return mobileno;
+    }
+
+    public void setMobileno(String mobileno) {
+        this.mobileno = mobileno;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getVillage() {
+        return village;
+    }
+
+    public void setVillage(String village) {
+        this.village = village;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
 
     public Integer getId() {
         return id;
@@ -39,4 +132,5 @@ public class FestUser {
     public void setPassword(String password) {
         this.password = password;
     }
+
 }
