62671b5c89f42117ec5432100e95ec52cb762b5e
Vinay Kumar J <jvkumar2003@gmail.com>
2025-06-28 19:47:42 +0530

    change

--- src/main/java/com/fest/backend/UserController.java
+++ src/main/java/com/fest/backend/UserController.java
@@ -1,11 +1,21 @@
 package com.fest.backend;
 
 import org.springframework.web.bind.annotation.*;
+@CrossOrigin(
+        origins = {
+                "https://miracle-fest-website-bimu.vercel.app",
+                "http://localhost:5173"
+        },
+        allowedHeaders = "*",
+        methods = {
+                RequestMethod.GET,
+                RequestMethod.POST,
+                RequestMethod.PUT,
+                RequestMethod.PATCH,
+                RequestMethod.DELETE
+        }
+)
 
-@CrossOrigin( origins = {
-        "https://miracle-fest-website-bimu.vercel.app" ,
-        "http://localhost:5173"
-                            })
 @RestController
 @RequestMapping("/api/auth")
 public class UserController {
